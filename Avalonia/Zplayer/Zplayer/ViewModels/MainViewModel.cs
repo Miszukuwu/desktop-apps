0318ff847d7bef7e0b9b169e1b92a0234d66735f
Miszuk <aleksanderowsinski23@gmail.com>
2024-11-21 14:35:05 +0100

    Ui and Icons

--- Avalonia/Zplayer/Zplayer/ViewModels/MainViewModel.cs
+++ Avalonia/Zplayer/Zplayer/ViewModels/MainViewModel.cs
@@ -4,6 +4,5 @@ namespace Zplayer.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
+    public int Progress { get; }
 }
