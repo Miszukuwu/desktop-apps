b289712a95e92ee7d07de223ea749d37d69b4f9a
Miszukuwu <aleksanderowsinski23@gmail.com>
2025-09-18 10:38:06 +0000

    double linked list

---
+++ C++/doubly_linked_list/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+struct Student{
+    string firstName;
+    int heightCM;
+    Student *next, *previous;
+};
+
+void printListA(Student* head){
+    Student* temp = head;
+    cout<<"Lista uczniow: "<<endl;
+    while (temp!=NULL){
+        cout<<temp->firstName<<" - "<<temp->heightCM<<" cm"<<endl;
+        temp = temp->next;
+    }
+}
+int main()
+{
+    Student* head = NULL;
+    do {
+        Student* student = new Student();
+        cout<<"Podaj imie: ";
+        cin>>student->firstName;
+        cout<<"Podaj wzrost (cm): ";
+        cin>>student->heightCM;
+        if (head==NULL) {
+            student->next = NULL;
+            student->previous = NULL;
+            head = student;
+        } else {
+            Student* temp = head;
+            while (temp->next!=NULL){
+                temp = temp->next;
+            }
+            student->previous = temp;
+            temp->next = student;
+        }
+        char choice;
+        cout<<"Czy chcesz wstawic kolejnego ucznia? (t/n): ";
+        cin>>choice;
+        if (choice!='t'){
+            break;
+        }
+    } while (true);
+    printListA(head);
+    return 0;
+}
